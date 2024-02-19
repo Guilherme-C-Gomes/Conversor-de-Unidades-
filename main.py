fbddeb8634684c64371aa9cca54cc465d69c9cb1
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-19 08:54:25 -0300

    criação da entrada de dados

---
+++ main.py
@@ -0,0 +1,11 @@
+#entrada de dados
+print("Bem vindo ao conversor de medidas!")
+print("1 = conversor de temperatura")
+print("2 = conversor de altura")
+print("3 = conversor de peso")
+print("4 = conversor de distancia")
+
+entrada = int(input ("qual conversor irá usar?"))
+
+#redirecionamento apartir da entrada
+
