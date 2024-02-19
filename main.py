9448235693826680768e70352a5bad48dc9d0894
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-19 11:10:42 -0300

    acrecentando a apresentação do conversor

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-#entrada de dados
-print("Bem vindo ao conversor de medidas!")
-print("1 = conversor de temperatura")
-print("2 = conversor de altura")
-print("3 = conversor de peso")
-print("4 = conversor de distancia")
+while True:
+    #entrada de dados
+    print("Bem vindo ao conversor de medidas!")
+    print("1 = conversor de temperatura")
+    print("2 = conversor de altura")
+    print("3 = conversor de peso")
+    print("4 = conversor de distancia")
 
-entrada = input ("qual conversor irá usar?")
+    entrada = input ("qual conversor irá usar?")
 
-#redirecionamento a partir da entrada
-if entrada == "1":
-    exec(open("conversor_temperatura.py").read())
-elif entrada == "2":
-    exec(open("conversor_de_altura.py").read())
-elif entrada == "3":
-    exec(open("conversor_de_peso.py").read())
-elif entrada == "4":
-    exec(open("conversor_de_distancia").read())
-else:
-    print("Valor invalido")
+    #redirecionamento a partir da entrada
+    if entrada == "1":
+        exec(open("conversor_temperatura.py").read())
+        break
+    elif entrada == "2":
+        exec(open("conversor_de_altura.py").read())
+        break
+    elif entrada == "3":
+        exec(open("conversor_de_peso.py").read())
+        break
+    elif entrada == "4":
+        exec(open("conversor_de_distancia").read())
+        break
+    else:
+        print("Valor invalido")
 
