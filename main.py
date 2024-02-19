e586c5e1fe1d93772d668a3c2bf92b4b86e8b983
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-19 10:52:38 -0300

    correção de erro, entrada do arquivo conversor temperatura

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ print("2 = conversor de altura")
 print("3 = conversor de peso")
 print("4 = conversor de distancia")
 
-entrada = int(input ("qual conversor irá usar?"))
+entrada = input ("qual conversor irá usar?")
 
 #redirecionamento a partir da entrada
-if entrada == 1:
-    exec(open("conver_temperatura.py"))
+if entrada == "1":
+    exec(open("conversor_temperatura.py").read())
