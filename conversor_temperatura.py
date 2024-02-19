c2869ddb0632293b5b9229e52742c339cf874e04
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-19 12:53:39 -0300

    entrada de valor do conversor de temperatura

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -1,1 +1,13 @@
-print("esta indo")
+#apresentação do conversor
+print("__________________________________________")
+print("__Bem vindo ao conversor de temperatura__")
+print("__________________________________________")
+
+
+#entrada de valor
+print("1 = kelvin (K)")
+print("2 = Celsius (C)")
+print("3 = Fahrenhet (F)")
+
+valor1 = input ("Primeira unidade: ")
+valor2 = input ("converter para: ")
