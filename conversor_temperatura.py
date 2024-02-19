232739b50eb6c88dcac72d298375f0012d66bd59
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-19 16:20:35 -0300

    quebrando o input do primeiro valor em duas partes para facilitar coopreenção do usuario

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -4,20 +4,56 @@ print("__Bem vindo ao conversor de temperatura__")
 print("__________________________________________")
 
 
-#entrada de valor
+#primeiro valor de entrada de valor
 while True:
+    #cabeçalho de entrada
     print("1 = kelvin (K)")
     print("2 = Celsius (C)")
     print("3 = Fahrenhet (F)")
 
+    #entrda
     valor1 = input ("Primeira unidade: ")
-    valor2 = input ("converter para: ")
-
+    
+    #verificação de numero valido
     if valor1 == 1 or 2 or 3:
-        break
-    elif valor2 == 1 or 2 or 3:
         break
     else:
         print("______________________")
         print("____valor invalido____")
         print("______________________")
+
+    #entrada do valor de medida
+    if valor1 == "1":
+        kelvin_valor = input("Valor de Kelvin:")
+    elif valor1  == "2":
+        celsius_valor = input("Valor de Celsius")
+    elif valor1 == "3":
+        fahrenhet_valor = input("Valor de Fahrenhet")
+
+
+#segundo valor de entrada de valor
+while True:
+    #cabeçalho de entrada
+    print("1 = kelvin (K)")
+    print("2 = Celsius (C)")
+    print("3 = Fahrenhet (F)")
+
+    #entrda
+    valor2 = input ("converter para: ")
+
+
+    #verificação de numero valido
+    if valor2 == 1 or 2 or 3:
+        break
+    else:
+        print("______________________")
+        print("____valor invalido____")
+        print("______________________")
+
+    #entrada do valor de medida
+    if valor1 == "1":
+        kelvin_valor = input("Valor de Kelvin:")
+    elif valor1  == "2":
+        celsius_valor = input("Valor de Celsius")
+    elif valor1 == "3":
+        fahrenhet_valor = input("Valor de Fahrenhet")    
