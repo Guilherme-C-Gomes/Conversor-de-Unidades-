ebf9e3873984d5e6155fb68fc8e7bded62b1da34
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-20 15:27:35 -0300

    criado while no main.py para verificação de numero valido

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 while True:
-    #entrada de dados
-    print("Bem vindo ao conversor de medidas!")
-    print("1 = conversor de temperatura")
-    print("2 = conversor de altura")
-    print("3 = conversor de peso")
-    print("4 = conversor de distancia")
+    try:
+        #entrada de dados
+        print("Bem vindo ao conversor de medidas!")
+        print("1 = conversor de temperatura")
+        print("2 = conversor de altura")
+        print("3 = conversor de peso")
+        print("4 = conversor de distancia")
 
-    entrada = input ("qual conversor irá usar?")
+        entrada = float(input ("qual conversor irá usar?"))
 
-    #redirecionamento a partir da entrada
-    if entrada == "1":
-        exec(open("conversor_temperatura.py").read())
-        break
-    elif entrada == "2":
-        exec(open("conversor_de_altura.py").read())
-        break
-    elif entrada == "3":
-        exec(open("conversor_de_peso.py").read())
-        break
-    elif entrada == "4":
-        exec(open("conversor_de_distancia").read())
-        break
-    else:
-        print("______________________")
-        print("____valor invalido____")
-        print("______________________")
+        #redirecionamento a partir da entrada
+        if entrada == 1:
+            exec(open("conversor_temperatura.py").read())
+            break
+        elif entrada == 2:
+            exec(open("conversor_de_altura.py").read())
+            break
+        elif entrada == 3:
+            exec(open("conversor_de_peso.py").read())
+            break
+        elif entrada == 4:
+            exec(open("conversor_de_distancia").read())
+            break
+        else:
+            print("______________________")
+            print("____valor invalido____")
+            print("______________________")
+    except ValueError:
+        print("_______________________________________")
+        print("Erro: Por favor, insira apenas números.")
+        print("_______________________________________")
+
 
