3efb2ae2879004c63879cb025a41a8f65b4400b6
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-20 07:21:18 -0300

    comparando se os parametros são diferentes

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -3,57 +3,65 @@ print("__________________________________________")
 print("__Bem vindo ao conversor de temperatura__")
 print("__________________________________________")
 
-
-#primeiro valor de entrada de valor
 while True:
-    #cabeçalho de entrada
-    print("1 = kelvin (K)")
-    print("2 = Celsius (C)")
-    print("3 = Fahrenhet (F)")
+    #primeiro valor de entrada de valor
+    while True:
+        #cabeçalho de entrada
+        print("1 = kelvin (K)")
+        print("2 = Celsius (C)")
+        print("3 = Fahrenhet (F)")
 
-    #entrda
-    valor1 = input ("Primeira unidade: ")
-    
-    #verificação de numero valido
-    if valor1 == 1 or 2 or 3:
-        break
+        #entrda
+        valor1 = input ("Primeira unidade: ")
+        
+        #verificação de numero valido
+        if valor1 == 1 or 2 or 3:
+            break
+        else:
+            print("______________________")
+            print("____valor invalido____")
+            print("______________________")
+
+        #entrada do valor de medida
+        if valor1 == "1":
+            kelvin_valor = input("Valor de Kelvin:")
+        elif valor1  == "2":
+            celsius_valor = input("Valor de Celsius")
+        elif valor1 == "3":
+            fahrenhet_valor = input("Valor de Fahrenhet")
+
+
+    #segundo valor de entrada de valor
+    while True:
+        #cabeçalho de entrada
+        print("1 = kelvin (K)")
+        print("2 = Celsius (C)")
+        print("3 = Fahrenhet (F)")
+
+        #entrda
+        valor2 = input ("converter para: ")
+
+
+        #verificação de numero valido
+        if valor2 == 1 or 2 or 3:
+            break
+        else:
+            print("______________________")
+            print("____valor invalido____")
+            print("______________________")
+
+        #entrada do valor de medida
+        if valor1 == "1":
+            kelvin_valor = input("Valor de Kelvin:")
+        elif valor1  == "2":
+            celsius_valor = input("Valor de Celsius")
+        elif valor1 == "3":
+            fahrenhet_valor = input("Valor de Fahrenhet")    
+
+    #comparando se os parametros são diferentes
+    if valor1 == valor2:
+        print("_______________________________________________")
+        print("os parametros de conversão não podem ser iguais")
+        print("_______________________________________________")
     else:
-        print("______________________")
-        print("____valor invalido____")
-        print("______________________")
-
-    #entrada do valor de medida
-    if valor1 == "1":
-        kelvin_valor = input("Valor de Kelvin:")
-    elif valor1  == "2":
-        celsius_valor = input("Valor de Celsius")
-    elif valor1 == "3":
-        fahrenhet_valor = input("Valor de Fahrenhet")
-
-
-#segundo valor de entrada de valor
-while True:
-    #cabeçalho de entrada
-    print("1 = kelvin (K)")
-    print("2 = Celsius (C)")
-    print("3 = Fahrenhet (F)")
-
-    #entrda
-    valor2 = input ("converter para: ")
-
-
-    #verificação de numero valido
-    if valor2 == 1 or 2 or 3:
         break
-    else:
-        print("______________________")
-        print("____valor invalido____")
-        print("______________________")
-
-    #entrada do valor de medida
-    if valor1 == "1":
-        kelvin_valor = input("Valor de Kelvin:")
-    elif valor1  == "2":
-        celsius_valor = input("Valor de Celsius")
-    elif valor1 == "3":
-        fahrenhet_valor = input("Valor de Fahrenhet")    
